9b88d7c6b78d8871bf21624c616308621bb0a8e7
Vitalii Kohut <vkohut@intelliarts.com>
2026-04-01 13:47:36 +0300

    chore: refresh frontend tooling and dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
-function getAllowedDevOrigins() {
-  return ["192.168.8.155"];
-}
-
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
-  allowedDevOrigins: getAllowedDevOrigins(),
   serverExternalPackages: ["pg"],
 };
 
